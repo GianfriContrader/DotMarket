7794c184cee1ba18bc4a2de7870cbc7a0d8ff878
Souhail92 <s.lamzouri@contrader.it>
2023-01-02 14:53:01 +0100

    add frame tab: comment image kart order product

---
+++ Models/Comment.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DotMarket.Models
+{
+
+    public class Comment
+    {
+
+        private ILazyLoader _lazyLoader;
+        private Profile _profile;
+        private Product _product;
+
+        public Comment() { }
+
+        private Comment(ILazyLoader lazyLoader)
+        {
+            _lazyLoader = lazyLoader;
+        }
+
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsLike { get; set; }
+
+
+
+
+        public Profile Profile
+        {
+            get
+            {
+                return _lazyLoader.Load(this, ref _profile);
+            }
+
+            set
+            {
+                _profile = value;
+            }
+        }
+
+        public Product Product
+        {
+            get
+            {
+                return _lazyLoader.Load(this, ref _product);
+            }
+
+            set
+            {
+                _product = value;
+            }
+        }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,79 @@
+﻿using Azure;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotMarket.Models
+{
+
+    public class Product
+    {
+        private ILazyLoader LazyLoader;
+        private IEnumerable<Comment> _comments = new List<Comment>();
+        private IEnumerable<Tag> _tags = new List<Tag>();
+        private IEnumerable<Kart> _karts = new List<Kart>();
+
+        public Product() { }
+
+        private Product(ILazyLoader lazyLoader)
+        {
+            LazyLoader = lazyLoader;
+        }
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public string Description { get; set; }
+
+        public uint Quantity { get; set; }
+
+        public char Status { get; set; }
+
+        public uint Stars { get; set; }
+        
+
+
+
+        public IEnumerable<Comment> Comments
+        {
+            get
+            {
+                return LazyLoader.Load(this, ref _comments);
+            }
+
+            set
+            {
+                _comments = value;
+            }
+        }
+
+        public IEnumerable<Tag> Tags
+        {
+            get
+            {
+                return LazyLoader.Load(this, ref _tags);
+            }
+
+            set
+            {
+                _tags = value;
+            }
+        }
+
+        public IEnumerable<Kart> Karts
+        {
+            get
+            {
+                return LazyLoader.Load(this, ref _karts);
+            }
+
+            set
+            {
+                _karts = value;
+            }
+        }
+    }
+
+}
