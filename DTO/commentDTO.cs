96bce953901fb1cc0e4d7dfeced0561fc2732074
Batdan81 <d.battistelli@contrader.it>
2023-01-05 11:34:46 +0100

    addDto

--- DTO/commentDTO.cs
+++ DTO/commentDTO.cs
@@ -1,6 +1,38 @@
 ﻿namespace DotMarket.DTO
 {
-    public class CommentDTO
+    public class CommentBaseDTO
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsLike { get; set; }
+    }
+
+    public class InsertCommentDTO : CommentBaseDTO
+    {
+
+        public long ProfileId { get; set; }
+
+        public long ProductId { get; set; }
+    }
+
+    public class ModifyCommentDTO : CommentBaseDTO
+    {
+        public long Id { get; set; }
+
+        public int ProductId { get; set; }
+    }
+
+    public class CommentReadDTO : CommentBaseDTO
+    {
+        public int Id { get; set; }
+    }
+
+    public class CommentDTO : CommentReadDTO
     {
     }
+
 }
+
+
